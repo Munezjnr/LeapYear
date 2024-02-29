4c26e1a099a7999e1fec07be1f1b9a3e45b9fe30
Munezjnr <chinexjohnbosco@gmail.com>
2024-02-29 21:18:03 +0100

    Add project files.

---
+++ LeapYear/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace LeapYear
+{
+    internal class Program
+    {
+
+            static void Main(string[] args)
+            {
+                int currentYear = DateTime.Now.Year;
+                int leapYearsFound = 0;
+
+                Console.WriteLine("Next 20 leap years:");
+
+                while (leapYearsFound < 20)
+                {
+                    if (IsLeapYear(currentYear))
+                    {
+                        Console.WriteLine(currentYear);
+                        leapYearsFound++;
+                    }
+                    currentYear++;
+                }
+            }
+
+            static bool IsLeapYear(int year)
+            {
+                // A year is a leap year if it is divisible by 4,
+                // except for years that are divisible by 100,
+                // unless the year is also divisible by 400.
+                return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+            }
+        }
+
+    }
+
